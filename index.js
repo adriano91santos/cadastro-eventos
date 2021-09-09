2299250c81795f066151ecd223f96303d6265698
adriano91santos <adrian.sbueno@gmail.com>
2021-09-09 18:06:34 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var meuNome = "Adriano"
+var dataAtual = 09
+var dataEvento = 22
+var idadeParticipante = 15
+var participantes = ["Vanessa", "Richard", "Joel", "Carol"]
+var permitirCadastro = true
+
+if (dataEvento < dataAtual){
+    console.log("Evento não permitido por data inválida!")
+    permitirCadastro = false
+}
+
+if (idadeParticipante < 18){
+    console.log("Evento não permitido pela idade!")
+    permitirCadastro = false
+}
+
+if (participantes.length > 100){
+    console.log("Cadastro não permitido por ter excedido o limite de pessoas!")
+    permitirCadastro = false
+}
+
+if (permitirCadastro){
+    participantes.push(meuNome)
+    console.log("Cadastro realizado com sucesso!")
+}else{
+    console.log("Houve problemas para se cadastrar!")
+}
+
+console.log("Listando os participantes:")
+for(let i=0; i<participantes.length; i++){
+    console.log(participantes[i])
+}
